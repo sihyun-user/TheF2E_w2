313269392b8c59d78bc22d704ae4a19d17019e35
chsiyu <chsiyu8652@gmail.com>
2021-11-15 16:14:56 +0800

    netlify test

--- src/router.js
+++ src/router.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Home from './pages/Home.vue'
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/home' },
+    { path: '/home', component: Home },
     { path: '/:notFound(.*)', redirect: '/home' }
   ]
 })
